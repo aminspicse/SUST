0e207bbddb6ce875b7314c2b62eba364861e132d
Md. Al Amin <aminspicse@gmail.com>
2022-10-09 22:08:10 +0600

    first commit

---
+++ web/note/script.js
@@ -0,0 +1,66 @@
+// get data from html 
+const form = document.querySelector('form');
+const input = document.getElementById('item');
+const btn_del_submit = document.getElementById('submit');
+const btn_del_clear = document.getElementById('clear');
+const tbl = document.querySelector('table');
+
+// arry for store data 
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+
+const entryMaker = (text) => {
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  td.className = "cell";
+  const btn_del = document.createElement('button');
+  btn_del.className = "btn btn-outline-primary btnCell";
+  tr.appendChild(td);
+
+  btn_del.innerHTML = "delete";
+  btn_del.onclick = function(){
+    for (let i = 0; i < tbl.rows.length; i++) {
+      if (tbl.rows[i] === tr) {
+        itemsArray.splice(i,1);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        tbl.removeChild(tr);
+      }
+    }
+  };
+ 
+  td.appendChild(btn_del);
+  td.appendChild(document.createTextNode(text));
+  tbl.appendChild(tr);
+}
+
+itemsArray.forEach(item => {
+  entryMaker(item);
+});
+
+function submitNewEntry(){
+  if (input.value === "" || input.value[0] === " ") {
+    input.value = "";
+    return;
+  }
+  itemsArray.push(input.value);
+  localStorage.setItem('items', JSON.stringify(itemsArray));
+  entryMaker(input.value);
+  input.value = "";
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  submitNewEntry();
+});
+
+btn_del_submit.addEventListener('click', function(){
+  submitNewEntry();
+});
+
+btn_del_clear.addEventListener('click', function () {
+  localStorage.clear();
+  while (tbl.firstChild) {
+    tbl.removeChild(tbl.firstChild);
+  }
+  itemsArray = [];
+});
